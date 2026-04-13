474b9eb86394d22a96fe398097d247977cf3297d
Tuemian <simonmartin93@hotmail.com>
2026-04-13 18:36:55 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { initClientMonitoring } from "@/lib/monitoring";
+
+initClientMonitoring();
 
 createRoot(document.getElementById("root")!).render(<App />);
