40b8dd28ad385fbcbd1c5595ac9e091a3cea30ae
Tuemian <simonmartin93@hotmail.com>
2026-04-14 22:19:18 +0200

    Update conveyor-3d-library.ts

--- src/lib/conveyor-3d-library.ts
+++ src/lib/conveyor-3d-library.ts
@@ -478,7 +478,7 @@ export function resolveConveyor3DAssets(
     const mirrorScaleZ = config.motorPosition === 'left' ? -1 : 1;
     const dScale = variant.scale ?? [1, 1, 1];
     const directAngleDeg = config.motorPosition === 'right'
-      ? (270 - config.motorAngle) % 360
+      ? (config.motorAngle + 180) % 360
       : (config.motorAngle + 270) % 360;
     const directAngleRad = directAngleDeg * (Math.PI / 180);
     const baseRot = variant.rotation ?? [0, 0, 0];
